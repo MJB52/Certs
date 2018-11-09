c54f18edd167b0e152b6cca735f1ad744832993a
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2018-11-09 00:49:49 -0600

    Add project files.

---
+++ Certs/CAServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Certs
+{
+    class CAServer
+    {
+        //members
+        //RevocationList
+        //ListOfCerts
+
+        //methods
+        //WatchForRequest
+        //GenerateCert
+        //RevokeCert
+        //SendCert
+        //SignCert
+    }
+}
---
+++ Certs/RSA.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Certs
+{
+    interface IRSA
+    {
+        string GeneratePandQ();
+        BigInteger GenerateN();
+        void GenerateNTotient();
+        BigInteger GenerateE();
+        BigInteger GenerateD();
+    }
+    class RSA : IRSA
+    {
+        public void GenerateNTotient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GeneratePandQ()
+        {
+            return "";
+        }
+
+        public BigInteger GenerateD()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BigInteger GenerateE()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BigInteger GenerateN()
+        {
+            throw new NotImplementedException();
+        }
+        private bool CheckPrimality(BigInteger num)
+        {
+            for (int i = 0; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+        private BigInteger GetBigInteger()
+        {
+
+        }
+    }
+
+
+    class RSAData
+    {
+        BigInteger N { get; set; }
+        BigInteger E { get; set; }
+        BigInteger D { get; set; }
+    }
+}
