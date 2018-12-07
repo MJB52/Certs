9abbd3c3ccd548c753cec960ff1170255743dfb3
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2018-12-06 23:20:42 -0600

    still dont work

--- Certs/Program.cs
+++ Certs/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Numerics;
+using System.Text;
 
 namespace Certs
 {
@@ -12,6 +14,29 @@ namespace Certs
             var data = genCert.StrategyPattern();
             IRSA rsa = new RSA();
             var hash = Sha256.HashSha256("Hello World");
+            Console.WriteLine(hash);
+            var e = rsa.Encrypt(hash, data.E,data.N); // doesnt work right now idfk
+            var test = new TestObj();
+            test.thing = e;
+            string ser = JsonConvert.SerializeObject(test);
+            Console.WriteLine(ser);
+
+            var obj = JsonConvert.DeserializeObject<TestObj>(ser);
+            //obj.thing = obj.thing.Where(c => !c.Equals(0)).ToArray();
+            foreach (var thing in obj.thing)
+                Console.Write(thing);
+
+            var d = rsa.Decrypt(ser, data.D,data.N); // doesnt work right now idfk
+            foreach (var thing in d)
+                Console.Write(thing);
+            var test2 = new TestObj();
+            test2.thing = d;
+            //test2.thing = test2.thing.Where(c => !c.Equals(0)).ToArray();
+            ser = JsonConvert.SerializeObject(test);
+            Console.WriteLine(ser);
+            obj = JsonConvert.DeserializeObject<TestObj>(ser);
+            foreach (var thing in obj.thing)
+                Console.Write(Convert.ToByte(thing));
             //var e = rsa.Encrypt(hash, data.E, data.N); // doesnt work right now idfk
             //Console.WriteLine(e.ToString());
             //Console.WriteLine();
@@ -21,6 +46,6 @@ namespace Certs
     }
     public class TestObj
     {
-        public byte[] thing { get; set; }
+        public string thing { get; set; }
     }
 }
