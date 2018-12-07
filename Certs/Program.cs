0f516834d85e3ea0f37d3f17f99b86085c7989d6
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2018-12-06 23:56:17 -0600

    dum dum

--- Certs/Program.cs
+++ Certs/Program.cs
@@ -21,22 +21,12 @@ namespace Certs
             string ser = JsonConvert.SerializeObject(test);
             Console.WriteLine(ser);
 
-            var obj = JsonConvert.DeserializeObject<TestObj>(ser);
-            //obj.thing = obj.thing.Where(c => !c.Equals(0)).ToArray();
-            foreach (var thing in obj.thing)
-                Console.Write(thing);
-
             var d = rsa.Decrypt(ser, data.D,data.N); // doesnt work right now idfk
-            foreach (var thing in d)
-                Console.Write(thing);
             var test2 = new TestObj();
             test2.thing = d;
             //test2.thing = test2.thing.Where(c => !c.Equals(0)).ToArray();
-            ser = JsonConvert.SerializeObject(test);
+            ser = JsonConvert.SerializeObject(test2);
             Console.WriteLine(ser);
-            obj = JsonConvert.DeserializeObject<TestObj>(ser);
-            foreach (var thing in obj.thing)
-                Console.Write(Convert.ToByte(thing));
             //var e = rsa.Encrypt(hash, data.E, data.N); // doesnt work right now idfk
             //Console.WriteLine(e.ToString());
             //Console.WriteLine();
