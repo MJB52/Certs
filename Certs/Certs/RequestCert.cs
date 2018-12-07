1bff27d3db399405aed505a97dfca19d4e09ae3a
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2018-12-07 00:20:12 -0600

    yep

--- Certs/Certs/RequestCert.cs
+++ Certs/Certs/RequestCert.cs
@@ -6,5 +6,25 @@ namespace Certs
 {
     class RequestCert
     {
+        public RequestCert()
+        {
+            //Go get list of folks
+            //foreach(var thing in folks)
+            //  Console.WriteLine(thing);
+            //Console.WriteLine("Would you like to get a cert from any of these folks");
+            //var name = Console.ReadLine();
+            //var found = folks.FirstOrDefault(c => c.ToLower() == name.ToLower());
+            //while(found == null)
+            //{
+            //Console.WriteLine("Would you like to get a cert from any of these folks");
+            //name = Console.ReadLine();
+            //found = folks.FirstOrDefault(c => c.ToLower() == name.ToLower());
+            //}
+            //SendRequest(name);
+        }
+        private void SendRequest(string name)
+        {
+
+        }
     }
 }
